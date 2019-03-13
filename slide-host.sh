a8aef68eb5e58f4c9f74893b612c5e640f908468
Judd Maltin <judd@newgoliath.com>
2019-03-12 22:43:15 -0400

    smoother, faster, more loggy

--- slide-host.sh
+++ slide-host.sh
@@ -2,18 +2,20 @@
 
 if [ -z "$1" ];
 then
-  echo "$0 <project_name> <source_dir> <app_name>"
+  echo "$0 <project_name> <app_name> <source_dir>"
   exit
 fi
 
 PROJECT=${1:-slides}
-SOURCE_DIR=${2:-.}
-APPNAME=${3:-slides}
+APPNAME=${2:-slides}
+SOURCE_DIR=${3:-.}
 
+oc version
 oc new-project ${PROJECT}
 oc new-build nginx --binary=true --name ${APPNAME} -n ${PROJECT}
 oc start-build bc/${APPNAME} --from-dir=${SOURCE_DIR}  -n ${PROJECT} --wait=true
 oc new-app ${APPNAME} -n ${PROJECT}
 oc expose svc ${APPNAME} -n ${PROJECT}
 oc get route ${APPNAME} -n ${PROJECT}
+
 
